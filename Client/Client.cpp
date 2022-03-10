3b13127497154d547dff48f1f66b7756ecd71988
Lightningale <gyl.lakewood@gmail.com>
2022-03-09 20:18:48 -0500

    Add project files.

---
+++ Client/Client.cpp
@@ -0,0 +1,45 @@
+#include <windows.networking.sockets.h>
+#pragma comment(lib, "Ws2_32.lib")
+#include <fstream>
+#include <string>
+#include <iostream>
+#include <vector>
+#include "Packet.h"
+using namespace std;
+
+int main()
+{
+	//starts Winsock DLLs
+	WSADATA wsaData;
+	if ((WSAStartup(MAKEWORD(2, 2), &wsaData)) != 0) {
+		return 0;
+	}
+
+	//initializes socket. SOCK_STREAM: TCP
+	SOCKET ClientSocket;
+	ClientSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ClientSocket == INVALID_SOCKET) {
+		WSACleanup();
+		return 0;
+	}
+
+	//Connect socket to specified server
+	sockaddr_in SvrAddr;
+	SvrAddr.sin_family = AF_INET;						//Address family type itnernet
+	SvrAddr.sin_port = htons(27000);					//port (host to network conversion)
+	SvrAddr.sin_addr.s_addr = inet_addr("127.0.0.1");	//IP address
+	if ((connect(ClientSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr))) == SOCKET_ERROR) {
+		closesocket(ClientSocket);
+		WSACleanup();
+		return 0;
+	}
+
+
+	//closes connection and socket
+	closesocket(ClientSocket);
+
+	//frees Winsock DLL resources
+	WSACleanup();
+
+	return 1;
+}
---
+++ Server/Server.cpp
@@ -0,0 +1,65 @@
+#include <windows.networking.sockets.h>
+#include <iostream>
+#include <string>
+#include "../Client/Packet.h"
+#pragma comment(lib, "Ws2_32.lib")
+
+using namespace std;
+
+int main()
+{
+	//starts Winsock DLLs		
+	WSADATA wsaData;
+	if (WSAStartup(MAKEWORD(2, 2), &wsaData) != 0)
+		return 0;
+
+	//create server socket
+	SOCKET ServerSocket;
+	ServerSocket = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
+	if (ServerSocket == INVALID_SOCKET) {
+		WSACleanup();
+		return 0;
+	}
+
+	//binds socket to address
+	sockaddr_in SvrAddr;
+	SvrAddr.sin_family = AF_INET;
+	SvrAddr.sin_addr.s_addr = INADDR_ANY;
+	SvrAddr.sin_port = htons(27000);
+	if (bind(ServerSocket, (struct sockaddr*)&SvrAddr, sizeof(SvrAddr)) == SOCKET_ERROR)
+	{
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 0;
+	}
+
+	//listen on a socket
+	if (listen(ServerSocket, 1) == SOCKET_ERROR) {
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 0;
+	}
+
+
+	cout << "Waiting for client connection\n" << endl;
+
+	//accepts a connection from a client
+	SOCKET ConnectionSocket;
+	ConnectionSocket = SOCKET_ERROR;
+	if ((ConnectionSocket = accept(ServerSocket, NULL, NULL)) == SOCKET_ERROR) {
+		closesocket(ServerSocket);
+		WSACleanup();
+		return 0;
+	}
+
+	cout << "Connection Established" << endl;
+
+
+	closesocket(ConnectionSocket);	//closes incoming socket
+	closesocket(ServerSocket);	    //closes server socket	
+	WSACleanup();					//frees Winsock resources
+
+	getchar();
+
+	return 1;
+}
