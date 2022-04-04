207f41ce6b577d7623225cfb76efed25ead96d6a
Lightningale <gyl.lakewood@gmail.com>
2022-04-04 15:07:24 -0400

    Updated Unit Tests

---
+++ LiteChatTests/ClientTests.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+#include "../Client/ClientFunctions.h"
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using Moq;
+namespace LiteChatTests
+{
+	TEST_CLASS(ClientTests)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
